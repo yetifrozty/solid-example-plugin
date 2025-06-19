2855d75fc607e11ddb18c4a27e8496c96a4dd346
YetiFrozty <hilding.olsson@outlook.com>
2025-06-19 16:33:55 +0200

    Suspense testing

--- src/client/example.tsx
+++ src/client/example.tsx
@@ -1,17 +1,15 @@
-import { createResource, createSignal } from "solid-js";
+import { createResource, createSignal, Suspense } from "solid-js";
 import { useClientAPI } from "@yetifrozty/solid-plugin/client";
 
-const myAsyncLoadingFunction = (): Promise<string> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Hello, world!");
-    }, 1000);
-  });
+const myAsyncLoadingFunction = async (): Promise<string> => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+  const json = await response.json()
+  return json.title
 };
 
 export default function Example() {
   const [count, setCount] = createSignal(0);
-  const [getData, setData] = createResource(myAsyncLoadingFunction, {deferStream: true});
+  const [getData, setData] = createResource(myAsyncLoadingFunction);
   // console.log(getData());
   const clientAPI = useClientAPI();
   const url = () => clientAPI.serverData.url;
@@ -22,7 +20,9 @@ export default function Example() {
     <button onClick={() => setCount(count() + 1)}>Click me</button>
     <p>Count: {count()}</p>
     <p>URL: {url()}</p>
-    <p>Data: {getData()}</p>
+    <Suspense fallback={<div>Loading...</div>}>
+      <p>Data: {getData()}</p>
+    </Suspense>
     <p>Current History Index: {currentHistoryIndex()}</p>
     <a use:link href="/awd">Test</a>
   </div>
